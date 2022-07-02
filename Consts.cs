30bc1a8977e3fe21cd58c6464cbeae691695b994
Yegor Kharitonchik <egorkhary@mail.ru>
2022-07-02 20:18:05 +0300

    Add files via upload

---
+++ Consts.cs
@@ -0,0 +1,53 @@
+﻿namespace Integers
+{
+    public static class Consts
+    {
+        public static int ReturnLargestInteger()
+        {
+            // TODO #3-1. Return the largest possible value of an int.
+            return int.MaxValue;
+        }
+
+        public static int ReturnSmallestInteger()
+        {
+            // TODO #3-2. Return the smallest possible value of an int.
+            return int.MinValue;
+        }
+
+        public static uint ReturnLargestUnsignedInteger()
+        {
+            // TODO #3-3. Return the largest possible value of an uint.
+            return uint.MaxValue;
+        }
+
+        public static uint ReturnSmallestUnsignedInteger()
+        {
+            // TODO #3-4. Return the smallest possible value of an uint.
+            return uint.MinValue;
+        }
+
+        public static long ReturnLargestLongInteger()
+        {
+            // TODO #3-5. Return the largest possible value of a long.
+            return long.MaxValue;
+        }
+
+        public static long ReturnSmallestLongInteger()
+        {
+            // TODO #3-6. Return the smallest possible value of a long.
+            return long.MinValue;
+        }
+
+        public static ulong ReturnLargestUnsignedLongInteger()
+        {
+            // TODO #3-7. Return the largest possible value of an ulong.
+            return ulong.MaxValue;
+        }
+
+        public static ulong ReturnSmallestUnsignedLongInteger()
+        {
+            // TODO #3-8. Return the smallest possible value of an ulong.
+            return ulong.MinValue;
+        }
+    }
+}
---
+++ Integers.cs
@@ -0,0 +1,55 @@
+﻿// ReSharper disable ConvertToConstant.Local
+
+namespace Literals
+{
+    public static class Integers
+    {
+        public static int ReturnInteger11()
+        {
+            // TODO #1-1. Return "0" literal.
+            return 0;
+        }
+
+        public static int ReturnInteger12()
+        {
+            // TODO #1-2. Return "1" literal.
+            return 1;
+        }
+
+        public static int ReturnInteger13()
+        {
+            // TODO #1-3. Return "-1" literal.
+            return -1;
+        }
+
+        public static int ReturnInteger14()
+        {
+            // TODO #1-4. Return "2,147,483,647" literal.
+            return 2_147_483_647;
+        }
+
+        public static int ReturnInteger15()
+        {
+            // TODO #1-5. Return "-2,147,483,648" literal.
+            return -2_147_483_648;
+        }
+
+        public static uint ReturnInteger16()
+        {
+            // TODO #1-6. Return "1" literal.
+            return 1u;
+        }
+
+        public static uint ReturnInteger17()
+        {
+            // TODO #1-7. Return "32,767" literal.
+            return 32_767u;
+        }
+
+        public static uint ReturnInteger18()
+        {
+            // TODO #1-8. Return "2,147,483,647" literal.
+            return 2_147_483_647u;
+        }
+    }
+}
